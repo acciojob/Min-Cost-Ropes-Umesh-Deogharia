03d6c2e306098a30376ad22cbf867ab920b554c1
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-08-10 15:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let sum = 0;
+  let cost = arr.map(element);
+	sum += element;
+	return sum;
 }
 
 module.exports=mincost;
