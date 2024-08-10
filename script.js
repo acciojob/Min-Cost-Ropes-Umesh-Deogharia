2b285f10a38f55a103eb860311bdb3d724d9b379
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-08-10 21:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-	let sum = 0;
-  let cost = arr.map(element);
-	sum += element;
-	return sum;
+
+	arr.sort((a,b)=>a-b);
+	let totalcost = 0;
+
+	while (arr.length>1) {
+		let rope1 = arr.shift();
+		let rope2 = arr.shift();
+		
+		let newropes = rope1 + rope2;
+		totalcost += newropes;
+		let i = 0;
+		while (i<arr.length && arr[i]<newropes) {
+			i++
+		}
+			arr.spice(i,0,newropes);
+	}
 }
 
 module.exports=mincost;
